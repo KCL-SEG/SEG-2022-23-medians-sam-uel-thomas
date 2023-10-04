15a18fa8f409dfd31a2b4dcc9dedadc03e3af71a
Sam Thomas <samuel.mmthomas@gmail.com>
2023-10-04 13:59:56 +0100

    Add functioning median return code

--- median.py
+++ median.py
@@ -1,12 +1,27 @@
 """Median calculator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+def median(numbers):
+    numbers.sort()
+
+    if len(numbers) % 2 == 0:
+        left_middle = len(numbers) // 2 - 1
+        right_middle = len(numbers) // 2
+        middle = (numbers[left_middle] + numbers[right_middle]) / 2
+        return(middle)
+    
+    else:
+        return(numbers[len(numbers) // 2])
+
+
 while True:
     try:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
+        print("The median is: ", median(numbers))
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+
+
